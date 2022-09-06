b4b74b1543848279a7893b42e359e1c5cfc8bfee
Leandro Milke Cavalheiro <leandromilkecavalheiro@gmail.com>
2022-09-06 10:13:37 -0300

    Criada classes, repositório e menu

---
+++ Projeto Aluguel Veiculo/Projeto Aluguel Veiculo/src/Main.java
@@ -0,0 +1,44 @@
+import menu.Menu;
+import repository.AdministradorRepository;
+import repository.ClienteRepository;
+import repository.VendedorRepository;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        AdministradorRepository admRepository = new AdministradorRepository();
+        ClienteRepository clienteRepository = new ClienteRepository();
+        VendedorRepository vendedorRepository = new VendedorRepository();
+        VendedorRepository veiculoRepository = new VendedorRepository();
+
+
+        boolean continua = true;
+
+        do {
+            Menu.menu1();
+            int opcao1 = sc.nextInt();
+            switch (opcao1){
+                case 1:
+                    Menu.menuCliente1();
+                    break;
+                case 2:
+                    Menu.menuVendedor1();
+                    break;
+                case 3:
+                    Menu.menuAdministrador1();
+                    break;
+                case 0:
+                    continua = false;
+                    break;
+                default:
+                    System.out.println("Alternatva inválida. Tente novamente !!!");
+                    break;
+            }
+        }while (continua);
+
+
+    }
+}
---
+++ Projeto Aluguel Veiculo/Projeto Aluguel Veiculo/src/model/Veiculo.java
@@ -0,0 +1,102 @@
+package model;
+
+import util.Contador;
+
+public class Veiculo {
+
+    private Integer id;
+
+    private String modelo;
+    private String marca;
+    private String cor;
+    private String placa;
+    private Tipo tipo;
+    private double valorLocacao;
+    private Status status;
+
+
+    public Veiculo(String modelo, String marca, String cor, String placa, String tipo, double valorLocacao) {
+        this.id = Contador.proximoId();
+
+        this.modelo = modelo;
+        this.marca = marca;
+        this.cor = cor;
+        this.placa = placa;
+        this.tipo = Tipo.valueOf(tipo.toUpperCase());
+        this.valorLocacao = valorLocacao;
+        this.status = Status.LIVRE;
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValorLocacao() {
+        return valorLocacao;
+    }
+
+    public void setValorLocacao(double valorLocacao) {
+        this.valorLocacao = valorLocacao;
+    }
+
+
+
+
+    public enum Tipo {
+        CARRO,
+        CAMINHAO,
+        MOTO
+    }
+
+    public enum Status {
+        LIVRE,
+        ALUGADO
+    }
+
+}
